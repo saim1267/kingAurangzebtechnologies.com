82269febca9124f13dc854be32e1dfda47f3d99e
saim1267 <93077464+saim1267@users.noreply.github.com>
2022-09-12 22:15:40 -0700

    king Aurangzeb Technologies

---
+++ WebApplication1/dbo.Table.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Table]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [name] VARCHAR(50) NULL, 
+    [email] VARCHAR(50) NULL, 
+    [Subject] VARCHAR(50) NULL, 
+    [Message] VARCHAR(500) NULL
+)
